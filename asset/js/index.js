3fd4c62a6c098f33db3791b24e68c11c92419308
Alessandro Aprile <aprile.alessandro@gmail.com>
2020-09-28 00:12:32 +0200

    AA adding one empty div for each city

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,24 +1,21 @@
 import "regenerator-runtime/runtime";
-import startOfWeek from "date-fns/startOfWeek";
-import endOfWeek from "date-fns/endOfWeek";
+// import startOfWeek from "date-fns/startOfWeek";
+// import endOfWeek from "date-fns/endOfWeek";
 import fromUnixTime from 'date-fns/fromUnixTime'
 import images from '../img/*.jpg'
 const imgPath='../img';
 
 
-const today = new Date();
-const startDay = startOfWeek(today);
-const endDay = endOfWeek(today);
-console.log(today, startDay, endDay);
+// const today = new Date();
+// const startDay = startOfWeek(today);
+// const endDay = endOfWeek(today);
+// console.log(today, startDay, endDay);
 
 const openWeatherApiKey = "61041e80233a8a40dbfc8a80a89dc295";
 
 const openWeatherBasepath = "https://api.openweathermap.org";
-// const path = '/data/2.5/weather'
 const openWeatherPath = "/data/2.5";
 const openWeatherEndPointF = "/onecall"
-const openWeatherEndPointW = "/onecall"
-const openWeatherCity = "q";
 const openWeatherLat = "lat";
 const openWeatherLon = "lon";
 const openWeatherAppId = "appid";
@@ -28,45 +25,36 @@ const defaultCityList = [
     cityName: "los angeles",
     lat: 45.4642,
     lon: -118.243683,
-    img: `LosAngeles`
-    //      ../img/LosAngeles.jpg
+    img: 'LosAngeles'
   },
   {
     cityName: "san francisco",
     lat: 37.773972,
     lon: -122.431297,
-    img: `${imgPath}/sanFrancisco.jpg`
+    img: 'sanFrancisco'
   },
   {
     cityName: "milano",
     lat: 45.4642,
     lon: 9.19,
-    img: `${imgPath}/milano.jpg`
+    img: `milano`
   },
   {
     cityName: "roma",
     lat: 41.902782,
     lon: 12.496366,
-    img: `${imgPath}/roma.jpg`
+    img: `roma`
   },
 ];
-
-const weatherDataStructure = {};
 
 async function getWeatherData(citynamesArray, wApiK) {
   let resultArray = [];
 
-  let resultCord = [
-    {
-      city: "",
-      lat: 0,
-      lon: 0
-    }
-  ];
-
   for (let i = 0; i < citynamesArray.length; i++) {
+
     const city = citynamesArray[i];
-    let forecastUrl= `${openWeatherBasepath}${openWeatherPath}${openWeatherEndPointF}?${openWeatherLat}=${city.lat}&${openWeatherLon}=${city.lon}&${openWeatherAppId}=${wApiK}&units=metric&exclude=minutely`;
+
+    let forecastUrl= `${openWeatherBasepath}${openWeatherPath}${openWeatherEndPointF}?${openWeatherLat}=${city.lat}&${openWeatherLon}=${city.lon}&${openWeatherAppId}=${wApiK}&units=metric&exclude=minutely,hourly`;
     let wUrl = ``;
     console.log(`fetching city ${city.cityName}`);
     try {
@@ -79,13 +67,11 @@ async function getWeatherData(citynamesArray, wApiK) {
         temp : res.current.temp,
         min : res.daily[0].temp.min,
         max : res.daily[0].temp.max,
-        // max : res.daily.filter(d => d.dt===res.current.dt).temp.max,
         week: []
       }
       for (let wi = 0; wi < res.daily.length; wi++) {
         const dayEl = res.daily[wi];
         const dtn2 = new Date(fromUnixTime(dayEl.dt));
-        console.log(dtn2.getDay())
 
         let day='';
         switch (dtn2.getDay()) {
@@ -130,7 +116,17 @@ async function getWeatherData(citynamesArray, wApiK) {
   console.log(resultArray);
   console.log(citynamesArray);
 
-  document.getElementById('bkg1').style.backgroundImage = `url(${images[citynamesArray[0].img]})`;
+  // document.getElementById('bkg1').style.backgroundImage = `url(${images[citynamesArray[0].img]})`;
+  const swiperContainer = document.getElementsByClassName('swiper-wrapper');
+
+  for (let bkgI = 0; bkgI < citynamesArray.length; bkgI++) {
+    const element = citynamesArray[bkgI];
+    // `<div id="bkg${[bkgI]}" class="background-img swiper-slide"></div>`
+    const el = document.createElement('div'); // , { is: `bkg${[bkgI]}`}
+    swiperContainer[0].insertBefore(el, null);
+
+
+  }
 
 
 }
