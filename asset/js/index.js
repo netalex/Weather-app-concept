4d9b380e22aa906549be9bd5e34e12e26c0b03c6
Alessandro Aprile <aprile.alessandro@gmail.com>
2020-09-27 12:10:58 +0200

    removing .cache from git

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,16 +1,14 @@
 const apiKey = "61041e80233a8a40dbfc8a80a89dc295";
 
-const list = document.querySelector(".weather-card .city-list");
-const listItems = list.querySelectorAll(".weather-card .city");
-const listItemArray = Array.from(listItems);
+// const list = document.querySelector(".weather-card .city-list");
+// const listItems = list.querySelectorAll(".weather-card .city");
+// const listItemArray = Array.from(listItems);
+// console.log(listItemArray, listItems, list);
 
 const defaultCityNames = ['los angeles','san francisco','milano', 'roma', 'torino'];
 
-console.log(listItemArray, listItems, list);
 
-let cityName = document.querySelector('.city-name');
-
-const inputVal = "los angeles";
+const inputVal = "";
 
 const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
 
