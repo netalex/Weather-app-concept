113bf29db0d5e577c490a153cb964c60688c1c24
Alessandro Aprile <aprile.alessandro@gmail.com>
2020-09-28 03:55:22 +0200

    AA data change working for today card

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,10 +1,9 @@
 import "regenerator-runtime/runtime";
 // import startOfWeek from "date-fns/startOfWeek";
 // import endOfWeek from "date-fns/endOfWeek";
-import fromUnixTime from 'date-fns/fromUnixTime'
-import images from '../img/*.jpg'
-const imgPath='../img';
-
+import fromUnixTime from "date-fns/fromUnixTime";
+import images from "../img/*.jpg";
+const imgPath = "../img";
 
 // const today = new Date();
 // const startDay = startOfWeek(today);
@@ -15,7 +14,7 @@ const openWeatherApiKey = "61041e80233a8a40dbfc8a80a89dc295";
 
 const openWeatherBasepath = "https://api.openweathermap.org";
 const openWeatherPath = "/data/2.5";
-const openWeatherEndPointF = "/onecall"
+const openWeatherEndPointF = "/onecall";
 const openWeatherLat = "lat";
 const openWeatherLon = "lon";
 const openWeatherAppId = "appid";
@@ -25,55 +24,54 @@ const defaultCityList = [
     cityName: "los angeles",
     lat: 45.4642,
     lon: -118.243683,
-    img: 'LosAngeles'
+    img: "LosAngeles"
   },
   {
     cityName: "san francisco",
     lat: 37.773972,
     lon: -122.431297,
-    img: 'sanFrancisco'
+    img: "sanFrancisco"
   },
   {
     cityName: "milano",
     lat: 45.4642,
     lon: 9.19,
-    img: `milano`
+    img: "milano"
   },
   {
     cityName: "roma",
     lat: 41.902782,
     lon: 12.496366,
-    img: `roma`
-  },
+    img: "roma"
+  }
 ];
 
 async function getWeatherData(citynamesArray, wApiK) {
   let resultArray = [];
 
   for (let i = 0; i < citynamesArray.length; i++) {
-
     const city = citynamesArray[i];
 
-    let forecastUrl= `${openWeatherBasepath}${openWeatherPath}${openWeatherEndPointF}?${openWeatherLat}=${city.lat}&${openWeatherLon}=${city.lon}&${openWeatherAppId}=${wApiK}&units=metric&exclude=minutely,hourly`;
+    let forecastUrl = `${openWeatherBasepath}${openWeatherPath}${openWeatherEndPointF}?${openWeatherLat}=${city.lat}&${openWeatherLon}=${city.lon}&${openWeatherAppId}=${wApiK}&units=metric&exclude=minutely,hourly`;
     let wUrl = ``;
     console.log(`fetching city ${city.cityName}`);
     try {
-      let res = await fetch(forecastUrl).then((response) => response.json());
+      let res = await fetch(forecastUrl).then(response => response.json());
       resultArray.push(res);
 
-      const td= {
+      const td = {
         date: res.current.dt,
         wType: res.current.weather[0].main,
-        temp : res.current.temp,
-        min : res.daily[0].temp.min,
-        max : res.daily[0].temp.max,
+        temp: res.current.temp,
+        min: res.daily[0].temp.min,
+        max: res.daily[0].temp.max,
         week: []
-      }
+      };
       for (let wi = 0; wi < res.daily.length; wi++) {
         const dayEl = res.daily[wi];
         const dtn2 = new Date(fromUnixTime(dayEl.dt));
 
-        let day='';
+        let day = "";
         switch (dtn2.getDay()) {
           case 0:
             day = "SUN";
@@ -101,14 +99,11 @@ async function getWeatherData(citynamesArray, wApiK) {
           dayName: day,
           wType: dayEl.weather[0].icon,
           min: dayEl.temp.min,
-          max: dayEl.temp.max,
+          max: dayEl.temp.max
         });
-
       }
 
       citynamesArray[i].today = td;
-
-
     } catch (err) {
       console.log("error: ", err);
     }
@@ -116,23 +111,60 @@ async function getWeatherData(citynamesArray, wApiK) {
   console.log(resultArray);
   console.log(citynamesArray);
 
-  // document.getElementById('bkg1').style.backgroundImage = `url(${images[citynamesArray[0].img]})`;
-  const swiperContainer = document.getElementsByClassName('swiper-wrapper');
+  const virtualSliderOptions = {
+    speed: 1000,
+    parallax: true,
+    grabCursor: true,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    on: {
+      init: () => {
+        document.querySelector('.city-name').innerHTML= citynamesArray[0].cityName;
+        document.querySelector('.w-type').innerHTML= citynamesArray[0].today.wType;
+        document.querySelector('.temp').innerHTML= (citynamesArray[0].today.temp).toFixed(1)+'<span>°</span>';
+        document.querySelector('.min-max').innerHTML=
+          (citynamesArray[0].today.min).toFixed(1)
+          +'<span>°</span>/'
+          +(citynamesArray[0].today.max).toFixed(1)
+          +'<span>°</span>';
+      }
+    },
+    virtual: {
+      slides: (function() {
+        let slides = [];
+        for (let bkgI = 0; bkgI < citynamesArray.length; bkgI++) {
+          slides.push(
+            `<img alt=${citynamesArray[bkgI].cityName} src=${images[
+              citynamesArray[bkgI].img
+            ]} data-id=${bkgI} id="bkg${bkgI}" class="background-img"></img>`
+          );
+        }
+        return slides;
+      })()
+    }
+  };
 
-  for (let bkgI = 0; bkgI < citynamesArray.length; bkgI++) {
-    const element = citynamesArray[bkgI];
-    // `<div id="bkg${[bkgI]}" class="background-img swiper-slide"></div>`
-    const el = document.createElement('div'); // , { is: `bkg${[bkgI]}`}
-    swiperContainer[0].insertBefore(el, null);
+  let mySwiper = new Swiper(".swiper-container", virtualSliderOptions);
 
+  mySwiper.on('slideChange', () => {
+    document.querySelector('.city-name').innerHTML= citynamesArray[mySwiper.activeIndex].cityName;
+    document.querySelector('.w-type').innerHTML= citynamesArray[mySwiper.activeIndex].today.wType;
+    document.querySelector('.temp').innerHTML= (citynamesArray[mySwiper.activeIndex].today.temp).toFixed(1)+'<span>°</span>';
+    document.querySelector('.min-max').innerHTML=
+      (citynamesArray[mySwiper.activeIndex].today.min).toFixed(1)
+      +'<span>°</span>/'
+      +(citynamesArray[mySwiper.activeIndex].today.max).toFixed(1)
+      +'<span>°</span>';
 
-  }
+  })
+
 
 
 }
 
 window.onload = function() {
-  getWeatherData(defaultCityList, openWeatherApiKey).then(
-    console.log("done")
-  );
+  getWeatherData(defaultCityList, openWeatherApiKey).then(console.log("done"));
 };
